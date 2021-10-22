7cfef45d94c98211d09d38b9731d43c5e89ee513
Pera <esperanza.nave@gmail.com>
2021-10-21 19:03:18 -0600

    Se agego datos a State

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -2,5 +2,25 @@
 
 export default () =>({
    isLoading: true,
-   entries: [1,2,3,4,5]
+   entries: [
+      {
+         id: new Date().getTime(), //1237129831
+         date: new Date().toDateString(), // sat 23, julio
+         text: 'Minim excepteur incididunt in mollit. Qui do pariatur est Lorem magna occaecat consectetur nulla excepteur. Irure ut aute ullamco culpa magna cillum velit ut.',
+         picture: null,
+      },
+      {
+         id: new Date().getTime() + 1000, //1237129831
+         date: new Date().toDateString(), // sat 23, julio
+         text: 'Consequat ad velit veniam non eiusmod dolor culpa voluptate in voluptate. Laboris ea qui aliquip adipisicing elit nulla sint non cillum consectetur dolor occaecat in ad. Ad fugiat deserunt occaecat sit commodo duis et exercitation non minim eu Lorem minim laborum. Fugiat reprehenderit eiusmod officia proident. Aliquip qui tempor veniam sit Lorem ipsum duis.',
+         picture: null,
+      },
+      {
+         id: new Date().getTime() + 2000, //1237129831
+         date: new Date().toDateString(), // sat 23, julio
+         text: 'Irure veniam duis est magna nisi. Velit sunt minim incididunt dolore duis laborum cillum laborum. Minim in enim magna aute nostrud enim aliquip sunt non eiusmod pariatur.',
+         picture: null,
+      },
+   ]
 })
+
